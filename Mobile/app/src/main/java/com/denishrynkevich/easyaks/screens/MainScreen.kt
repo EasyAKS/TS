c2ab8f9746093ebcaaa377a5686641dfb8da6232
mybagfull <baezgamer01@gmail.com>
2024-04-25 17:59:48 +0300

    Theme choice in MainScreen

--- Mobile/app/src/main/java/com/denishrynkevich/easyaks/screens/MainScreen.kt
+++ Mobile/app/src/main/java/com/denishrynkevich/easyaks/screens/MainScreen.kt
@@ -2,14 +2,21 @@ package com.denishrynkevich.easyaks.screens
 
 import android.util.Log
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.denishrynkevich.easyaks.MainViewModel
 import com.denishrynkevich.easyaks.navigation.Screens
@@ -22,22 +29,22 @@ fun MainScreen(navController: NavController, viewModel: MainViewModel) {
     themes.forEach {
         Log.d("CheckData", "THEME: ${it.themeName}")
     }
-    /*Column(
+    Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Text("Choose a theme:")
         Spacer(modifier = Modifier.height(16.dp))
-        themeNames.forEach { theme ->
+        themes.forEach { theme ->
             Button(onClick = {
-                navController.navigate("quiz_screen/${theme}")
+                navController.navigate("quiz_screen/${theme.themeName}")
             }) {
-                Text(theme)
+                Text(theme.themeName.orEmpty())
             }
             Spacer(modifier = Modifier.height(16.dp))
         }
-    }*/
+    }
 
     // Rest of your code displaying a button
     Button(
